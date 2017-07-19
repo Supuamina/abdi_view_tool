919e2000c4d4c78ed76e2e39e26d5bad1affcc4b
slcwestdevcamp <slcwest@devcamp.com>
2017-07-19 14:33:49 +0000

    Updated gitignore file and read me

--- lib/abdi_view_tool.rb
+++ lib/abdi_view_tool.rb
@@ -1,6 +1,2 @@
 require "abdi_view_tool/version"
 require "abdi_view_tool/renderer"
-
-module AbdiViewTool
-  # Your code goes here...
-end
---
+++ lib/abdi_view_tool/renderer.rb
@@ -0,0 +1,7 @@
+module AbdiViewTool
+  class Renderer
+      def self.copyright name, msg
+          "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+      end
+  end
+end
